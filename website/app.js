0116b61e2344f091da167f21e870cc56ec7853a5
Natalia Romanyuk <feliz@MacBook-Pro.modem>
2020-11-05 18:35:19 +1100

    Get data from Open weather

--- website/app.js
+++ website/app.js
@@ -1,5 +1,14 @@
 /* Global Variables */
+const apiEndPoint = "api.openweathermap.org";
+const apiKey = "081a059f88749d9d3c5a61e73985ef20";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+function getData() {
+  fetch(`http://${apiEndPoint}/data/2.5/weather?zip=94040,us&appid=${apiKey}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+getData();
